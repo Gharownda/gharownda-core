c13ff3280accdb59130aeedad9a0b8b83bb9d4f6
Faheem Ul Islam <70851748+faheemKamboh@users.noreply.github.com>
2026-09-15 18:45:25 +0500

    Add core public API

---
+++ lib/gharownda/core.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "core/version"
+require_relative "core/provenance/reference"
+require_relative "core/provenance/edge"
+require_relative "core/provenance/graph"
+
+module Gharownda
+  module Core
+  end
+end
